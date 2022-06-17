8bc88d5e0f46d087fde2ccd8a98ffe085f2fe37e
joscasvaz <jicastrotic99@gmail.com>
2022-06-17 13:51:32 +0200

    feat: Vertex/Edge
    Implementation of Vertex and Edge classes with their respective methods.
    Non-tested version.

--- PI6_joscasvaz/src/ejercicio4/EdgeEjercicio4.java
+++ PI6_joscasvaz/src/ejercicio4/EdgeEjercicio4.java
@@ -6,8 +6,8 @@ public record EdgeEjercicio4 (VertexEjercicio4 source, VertexEjercicio4 target, 
 		Double weight) implements SimpleEdgeAction<VertexEjercicio4, Integer> {
 
 	public static EdgeEjercicio4 of(VertexEjercicio4 v1, VertexEjercicio4 v2, Integer acc) {
-		//TODO
-		Double w = (double) acc * 0;
+		
+		Double w = (double) acc * DatosEjercicio4.capCont(v1.index());
 		return new EdgeEjercicio4(v1, v2, acc, w);
 	}
 	
--- PI6_joscasvaz/src/ejercicio4/VertexEjercicio4.java
+++ PI6_joscasvaz/src/ejercicio4/VertexEjercicio4.java
@@ -1,27 +1,91 @@
 package ejercicio4;
 
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import ejercicio4.DatosEjercicio4.Contenedor;
+import us.lsi.common.List2;
+import us.lsi.graphs.virtual.VirtualVertex;
 
 
-public class VertexEjercicio4 {
+public record VertexEjercicio4(Integer index, List<Integer> elRestantes, List<Integer> capRestante)
+	implements VirtualVertex<VertexEjercicio4, EdgeEjercicio4, Integer> {
 	
-	public static void iniDatos(String path) {
-		//TODO
+	static List<Integer> capIniciales = DatosEjercicio4.contenedores.stream()
+			.map(Contenedor::capacidad)
+			.collect(Collectors.toList());
+	
+	static List<Integer> elIniciales = IntStream.range(0, DatosEjercicio4.elementos.size())
+			.boxed()
+			.toList();
+	
+	public static VertexEjercicio4 of(Integer index, List<Integer> elRestantes, List<Integer> capRestante) {
+		return new VertexEjercicio4(index, elRestantes, capRestante);
+	}
+	
+	public static VertexEjercicio4 copy(VertexEjercicio4 v) {
+		return new VertexEjercicio4(v.index(), v.elRestantes(), v.capRestante());
 	}
 	
 	public static VertexEjercicio4 initialVertex() {
-		//TODO
-		return null;
+		return of(0, elIniciales, capIniciales);
 	}
 	
-	public static Predicate<VertexEjercicio4> goal(){
-		//TODO
-		return null;
+	@Override
+	public List<Integer> actions() {
+		
+		List<Integer> alternativas = new ArrayList<>();
+		
+		if(this.index() < DatosEjercicio4.elementos.size()) {
+			
+			alternativas = IntStream.range(0, this.elRestantes().size())
+					.boxed()
+					.toList();
+		}
+		
+		return alternativas;
+	}
+	
+	@Override
+	public VertexEjercicio4 neighbor(Integer a) {
+		
+		List<Integer> nuevosElRestantes = new ArrayList<>(elRestantes);
+		List<Integer> nuevasCapRestantes = List2.copy(capRestante);
+		
+		Integer cap = 100000;
+		Integer tam = 0;
+		
+		int ind = 0;
+		
+		if(DatosEjercicio4.elementos.size() < a && 0 <= a) {
+			
+			tam = DatosEjercicio4.elementos.get(this.elRestantes().get(a)).tam();
+			
+			int i = 0;
+			while(i < this.capRestante().size()) {
+				
+				Integer capSel = this.capRestante().get(ind);
+				
+				if(0 <= (capSel - tam)){
+					cap = Math.min(cap, capSel - tam);
+					ind = i;
+				}
+				
+				i++;
+			}
+			
+			nuevosElRestantes.remove(a);
+			nuevasCapRestantes.set(ind, cap);
+		}
+		
+		return of(index + 1, nuevosElRestantes, nuevasCapRestantes);
 	}
 
-	public int index() {
-		// TODO Auto-generated method stub
-		return 0;
+	@Override
+	public EdgeEjercicio4 edge(Integer a) {
+		VertexEjercicio4 neighbor = this.neighbor(a);
+		return EdgeEjercicio4.of(this, neighbor, a);
 	}
-
 }
